8867d8d39e11331c4f7a6b1b347e50cda5580c3f
Abinash Panda <abinash@prodios.com>
2026-09-15 17:21:08 +0530

    feat: create Dockerfile to setup docker

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,11 @@
+import type { CSSProperties } from 'react'
+
 export { cn } from 'cn'
+
+export type WithBasicProps<T = unknown> = T & {
+  className?: string
+  style?: CSSProperties
+}
 
 export function getErrorMessage(error: unknown) {
   if (error instanceof Error && error.message) return error.message
